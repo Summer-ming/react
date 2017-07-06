2cd4a1b02939a8d65d32f823e2716c3744e00551
luzhongkuan <zhongkuan.lu@vhuomai.com>
2017-07-06 10:09:24 +0800

    ''

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Button} from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button>按钮</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
